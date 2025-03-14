dafb892483b11ee8f481d24d7bf49f7fe0d80f5f
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-03-14 18:24:05 -0300

    layout

--- index.py
+++ index.py
@@ -16,7 +16,17 @@ df_data["Date"] = pd.to_datetime(df_data["Date"])
 
 #Layout
 app.layout = html.Div(children=[
+        html.H5("Cidades:"),
+        dcc.Checklist(df_data["City"].value_counts().index,
+                      df_data["City"].value_counts().index, id="check_city"),
 
+       html.H5("Variável de análise:"),
+       dcc.RadioItems(["gross income","Rating"], "gross income", id="main_variable"),
+
+       dcc.Graph(id="fig_city"),
+       dcc.Graph(id="pay_fig"),
+       dcc.Graph(id="income_per_product_fig"),
+    
     ]
 )
 
