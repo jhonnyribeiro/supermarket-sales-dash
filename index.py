e237e538511bde24ac78f2d18268a7d058a0cffb
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-03-19 12:47:10 -0300

    layout

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 
 # app = dash.Dash(__name__)
 app = dash.Dash(
-    external_stylesheets=[dbc.themes.DARKLY]
+    external_stylesheets=[dbc.themes.MINTY]
 )
 server = app.server
 
@@ -20,16 +20,40 @@ df_data["Date"] = pd.to_datetime(df_data["Date"])
 
 #Layout
 app.layout = html.Div(children=[
-        html.H5("Cidades:"),
-        dcc.Checklist(df_data["City"].value_counts().index,
-                      df_data["City"].value_counts().index, id="check_city"),
+        dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        html.H2("Conecta Tecnologia", style={"font-family": "Voltaire", "font-size": "23px"}),
+                        html.Hr(),
+                        
+                        html.H5("Cidades:"),
+                        dcc.Checklist(df_data["City"].value_counts().index,
+                                    df_data["City"].value_counts().index, id="check_city", inputStyle={"margin-right": "5px", "margin-left": "5px" }),
 
-       html.H5("Variável de análise:"),
-       dcc.RadioItems(["gross income","Rating"], "gross income", id="main_variable"),
+                        html.H5("Variável de análise:", style={"margin-top":"30px"}),
+                        dcc.RadioItems(["gross income","Rating"], "gross income", id="main_variable",
+                        inputStyle={"margin-right": "5px", "margin-left": "5px" }),
+                        ], style={"height": "90vh", "margin": "20px", "padding": "20px"})
+            ], sm=2),
 
-       dcc.Graph(id="city_fig"),
-       dcc.Graph(id="pay_fig"),
-       dcc.Graph(id="income_per_product_fig"),
+            dbc.Col([
+                dbc.Row([
+                    dbc.Col([dcc.Graph(id="city_fig")], sm=4),
+                    dbc.Col([dcc.Graph(id="gender_fig")], sm=4),
+                    dbc.Col([dcc.Graph(id="pay_fig")], sm=4),
+                ]),
+                dbc.Row([dcc.Graph(id="income_per_date_fig")]),
+                dbc.Row([dcc.Graph(id="income_per_product_fig")]),
+
+
+      
+             
+            ], sm=10),
+        ])
+
+
+
+
     
     ]
 )
@@ -59,9 +83,9 @@ def render_graphs(cities, main_variable):
     fig_payment= px.bar(df_payment, y="Payment", x=main_variable, orientation='h')
     fig_product_income  = px.bar(df_product_income, x=main_variable, y='Product line', color='City',  orientation='h', barmode='group')
 
-    fig_city.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=200, template='plotly_dark')
-    fig_payment.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=200, template='plotly_dark')
-    fig_product_income.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=500, template='plotly_dark')
+    fig_city.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=200)
+    fig_payment.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=200)
+    fig_product_income.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=500)
 
     return fig_city, fig_payment, fig_product_income
     
