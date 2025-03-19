42640a6264155fb461736cff7244ca97a5ff4ed4
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-03-19 12:21:22 -0300

    layout

--- app.py
+++ app.py
@@ -5,11 +5,35 @@ import dash
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
+card_content = [
+    dbc.CardHeader("Card header"),
+    dbc.CardBody(
+        [
+            html.H5("Card title", className="card-title"),
+            html.P(
+                "This is some card content that we'll reuse",
+                className="card-text",
+            ),
+        ]
+    ),
+]
+
 app.layout = html.Div([
     dbc.Row([
-        dbc.Col(html.Div("Column"), style={"background": "#ff0000"}, md=6, sm=4),
-        dbc.Col(html.Div("Column"), style={"background": "#ff0ff0"}, md=3, sm=4),
-        dbc.Col(html.Div("Column"), style={"background": "#ffff00"}, md=3, sm=4),
+        dbc.Col([
+            dbc.Card(card_content, color='primary', inverse=True, style={"height": "90vh", "margin": "10px"}),
+        ], sm=4),
+        dbc.Col([
+            dbc.Row([
+                dbc.Col(dbc.Card(card_content, color='info', inverse=True),),
+                dbc.Col(dbc.Card(card_content, color='info', inverse=True),),
+                ]),
+            dbc.Row([
+                dbc.Col(dbc.Card(card_content, color='error', inverse=False), md=4),
+                dbc.Col(dbc.Card(card_content, color='error', inverse=False), md=4),
+                dbc.Col(dbc.Card(card_content, color='error', inverse=False), md=4),
+                ]),
+        ]),
     ])
 ])
 
